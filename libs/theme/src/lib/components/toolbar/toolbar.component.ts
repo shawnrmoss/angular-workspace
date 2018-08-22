203bd38f3cb08968253c7a344bde434bcfb05c9b
Shawn Moss <mosss@fcc.ca>
2018-08-22 11:32:04 -0600

    Tool bar shell work

--- libs/theme/src/lib/components/toolbar/toolbar.component.ts
+++ libs/theme/src/lib/components/toolbar/toolbar.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+  HostBinding
+} from '@angular/core';
 
 @Component({
   selector: 'angular-workspace-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @HostBinding('class') hostClasses = 'toolbar';
+  @Input('logo') logo: string;
+  @Input('icon') icon = 'menu';
+  @Input('loading') loading: false;
+  @Output() leftNavigationToggle = new EventEmitter();
+  @Output() rightNavigationToggle = new EventEmitter();
+
   constructor() {}
 
   ngOnInit() {}
+
+  toggleLeftSideNav() {
+    this.leftNavigationToggle.emit();
+  }
+
+  toggleRightSideNav() {
+    this.rightNavigationToggle.emit();
+  }
 }
